d7f5d7fd298f1aba786ffec2083d8da58af56a77
NaderYasser <naderdd7@gmail.com>
2019-05-09 01:17:59 +0200

    second requirements

--- main.js
+++ main.js
@@ -3,10 +3,11 @@
 
 var cat1_name=  "jessy";
 
+var cat2_name=  "Kiara";
 
 
 document.getElementById("cat1").innerHTML = cat1_name;
-
+document.getElementById("cat2").innerHTML = cat2_name;
 
 
 
@@ -22,4 +23,4 @@ function clicker(imgid ,spanid ) {
 }
 
 clicker("img1","count1");
-
+clicker("img2","count2");
